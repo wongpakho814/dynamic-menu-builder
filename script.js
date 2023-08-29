5e822d33d244ac4054dc345aff91ed409aafb14a
wongpakho814 <wongpakho814@gmail.com>
2023-08-29 16:21:32 +1000

    Added HTML and styling for Level 1 menu item and switch role button

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle sidebar
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+// End of toggle sidebar
